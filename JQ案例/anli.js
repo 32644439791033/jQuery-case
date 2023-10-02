bd3192b031b7c54b7582800888ab5a9b6f5d5a97
bukeliyu <3264443979@qq.com>
2023-10-02 13:29:11 +0800

    jq案例事件

---
+++ JQ案例/anli.js
@@ -0,0 +1,22 @@
+// $(document).ready(function(){
+//     $("p").click(function(){
+//       $(this).hide();
+//     });
+//   });隐藏
+// 鼠标
+// $("#p1").mouseenter(function(){
+//     alert('您的鼠标移到了 id="p1" 的元素上!');
+// });
+// $("#p1").mousedown(function(){
+//     alert("鼠标在该段落上按下！");
+// });
+// $("#p1").mouseup(function(){
+//     alert("鼠标在段落上松开。");
+// });
+// 聚焦
+// $("input").focus(function(){
+//     $(this).css("background-color","#cccccc");
+//   });
+//   $("input").blur(function(){
+//     $(this).css("background-color","#ffffff");
+//   });
